86e7eec16844651d31969e3db824db614f591fa6
Adrian11Faudoa <afa1823@gmail.com>
2025-09-18 22:43:23 -0600

    Update Structure Code

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,27 @@
 'use strict';
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
-
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
 
+  app.route('/api/issues/:project')
+  
+    .get(function (req, res){
+      let project = req.params.project;
+      
+    })
+    
+    .post(function (req, res){
+      let project = req.params.project;
+      
+    })
+    
+    .put(function (req, res){
+      let project = req.params.project;
+      
+    })
+    
+    .delete(function (req, res){
+      let project = req.params.project;
+      
+    });
+    
 };
